e49102e2b79f7dee6c55580e525c48aaa1e130f4
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-10-01 16:16:19 +1000

    Add __init__.py file.
    This is from PyGLy

---
+++ pymesh/__init__.py
@@ -0,0 +1,30 @@
+import pkgutil
+import os
+
+# the version of software
+# this is used by the setup.py script
+from version import __version__
+
+# make 'from module import *' work dynamically.
+# otherwise we have to manually update the __all__ list.
+# http://stackoverflow.com/questions/1057431/loading-all-modules-in-a-folder-in-python
+for module in os.listdir( os.path.dirname( __file__ ) ):
+    name, extension = os.path.splitext( module )
+
+    # don't import ourself
+    if name == '__init__':
+        continue
+
+    # we can import .py, .pyc and .pyo file types
+    extensions = [
+        '.py',
+        '.pyc',
+        '.pyo'
+        ]
+    if extension not in extensions: 
+        continue
+
+    # file type is ok, import it
+    __import__( name, locals(), globals() )
+    del module
+
