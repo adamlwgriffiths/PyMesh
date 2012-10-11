45e3a42ee6331a157873cc89bd627709f81de437
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-10-11 18:25:10 +1100

    Load multiple files.

--- pymesh/examples/obj/main.py
+++ pymesh/examples/obj/main.py
@@ -2,16 +2,25 @@ import os
 
 from pymesh.obj import OBJ
 
-
-def main():
+def load( filename ):
+	print 'Loading', filename
 	# get our current directory
 	path = os.path.join(
-            os.path.dirname( __file__ ),
-            '../data/obj/cessna.obj'
-            )
+        os.path.dirname( __file__ ),
+        '../data/obj',
+        filename
+        )
 
 	obj = OBJ()
 	obj.load( path )
+
+def main():
+	files = [
+		'cessna.obj',
+		'cornell_box.obj',
+		]
+	for filename in files:
+		load( filename )
 
 if __name__ == '__main__':
 	main()
