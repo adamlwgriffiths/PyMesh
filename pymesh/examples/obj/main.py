f309a94fc3c8e75fc014df2099e8e506d0f898e4
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-10-14 14:29:19 +1100

    Fix tabs in example.
    Damnit Sublime Editor! You were supposed to
    be the one!

--- pymesh/examples/obj/main.py
+++ pymesh/examples/obj/main.py
@@ -3,35 +3,35 @@ import os
 from pymesh.obj import OBJ
 
 def load( filename ):
-	print 'Loading', filename
-	# get our current directory
-	path = os.path.join(
+    print 'Loading', filename
+    # get our current directory
+    path = os.path.join(
         os.path.dirname( __file__ ),
         '../data/obj',
         filename
         )
 
-	obj = OBJ()
-	obj.load( path )
+    obj = OBJ()
+    obj.load( path )
 
 def main():
-	# load all .obj files in our data directory
-	# get the path relative to our examples file
-	path = os.path.join(
+    # load all .obj files in our data directory
+    # get the path relative to our examples file
+    path = os.path.join(
         os.path.dirname( __file__ ),
         '../data/obj'
         )
-	# get the directory contents
-	contents = os.listdir(path)
+    # get the directory contents
+    contents = os.listdir(path)
 
-	# iterate through the contents and load
-	# each file that is a .obj file
-	for filename in contents:
-		name, extension = os.path.splitext( filename )
-		
-		if extension.lower() == '.obj':
-			load( filename )
+    # iterate through the contents and load
+    # each file that is a .obj file
+    for filename in contents:
+        name, extension = os.path.splitext( filename )
+
+        if extension.lower() == '.obj':
+            load( filename )
 
 if __name__ == '__main__':
-	main()
+    main()
 
